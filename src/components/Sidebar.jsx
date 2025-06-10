2896010892079b33e53c46c9a16d489543155a43
Mayur <129158363+MayurArgade@users.noreply.github.com>
2025-06-10 20:37:45 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
   return (
     <div className={`floating-sidebar ${visible ? 'visible' : 'hidden'}`}>
       <a href="https://github.com/mayurargade" target="_blank" rel="noreferrer"><FaGithub /></a>
-      <a href="https://linkedin.com/in/mayurargade" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+      <a href="https://www.linkedin.com/in/mayur-argade-031b71271" target="_blank" rel="noreferrer"><FaLinkedin /></a>
       <a href="mailto:mayurargade423@gmail.com"><FaEnvelope /></a>
     </div>
   );
